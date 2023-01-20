f3065dddd80207c2928022ca559305e0f72f30d5
Johan Wulf <johanwulf98@gmail.com>
2023-01-20 17:17:39 +0100

    feat: add submit button

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,6 +11,11 @@ export const Input = () => {
         placeholder="Enter explanation"
         className="input w-full max-w-xs mt-2"
       />
+      <input
+        type="submit"
+        value="Submit"
+        className="input w-full max-w-xs mt-2"
+      />
     </>
   );
 };
